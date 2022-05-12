8e6d273a6bd49696f5245dbec93e1b4b00283a34
unknown <nevraev1109@mail.ru>
2022-05-12 16:50:11 +0500

    другой код

--- дз 17/Source.cpp
+++ дз 17/Source.cpp
@@ -1,53 +1,48 @@
 #include <iostream>
+
 using namespace std;
-#include <time.h>
-#include<algorithm>
-#include<stdlib.h>
-//задача 1
-int sum(int a =0, int b=0, int c=0, int d=0, int i=0) {
-	int res;
-	res = a + b + c + d + i;
-	return res;
-}
-
-
-		
-	
-// задача 2
-void printArray(int array[], int length) {
-	for (int i = 0; i < length; i++) {
-		array[i] = rand() % (10 - (-10)) + (-10);
-		if (array[i] < 0)
-			cout << array[i] * (-1) << " ";
-		else
-			cout << array[i]<<" ";
-	}
-}
-//задача 3
-int s(int n) {
-	static int s = 2;
-	return s * n;
-}
 
 int main() {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+	// Задача 1
 
-	// задача 1
-	cout << " задача 1\n";
-	int t = sum(2,6);
-	cout << t<<endl;
+	/*int input_min;
+	const int min_in_clok = 60;
+
+	cout << "input_min ";
+	cin >> input_min;
+	int clok = input_min / min_in_clok;
+	int min = input_min - clok * min_in_clok;
+	cout << clok << " clok " << min << " min";*/
 
 	// Задача 2
-	cout << " задача 2\n";
-	int arr[10];
-	printArray(arr, 10);
-	cout << endl;
-	// Задача 3 
-	cout << " задача 3\n";
-	cout << s(4)<<endl;
-	cout << s(5)<<endl;
-	cout << s(10)<<endl;
+
+	/*int user_input;
+	const float One_inch = 2.54;
+	cout << "user input ";
+	cin >> user_input;
+	int diagonal = user_input * One_inch + 1; //округляем в большуу сторону при помощи +1
+	cout << "diagonal: " << diagonal;*/
+
+	//Задача 3
+
+	float Feed_for_dey; // количество корма в граммах
+	int Days_in_month = 30;
+	cout << "Feed_for_dey (g): ";
+	cin >> Feed_for_dey;
+	float Feed_for_month = Feed_for_dey * Days_in_month / 1000; // переводим граммы в килограммы делим на 1000
+	cout << "Feed for a month (kg): " << Feed_for_month;
+
+
+
+
+
+
+
+
+
+
 
 	return 0;
+
+
 }
